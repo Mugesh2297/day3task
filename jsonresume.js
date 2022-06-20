c88aa31e224e7eb902257ebb408536153d05eb19
Mugesh Kumar K <105528383+Mugesh2297@users.noreply.github.com>
2022-06-20 20:16:38 +0530

    Add files via upload

---
+++ jsonresume.js
@@ -0,0 +1,58 @@
+var resumeOfMugesh = [{
+    basicInfo:{
+        Name : "Mugesh Kumar K",
+        Age : 23,
+        email : "mugesh@gmail.com",
+        Phone : 9878912184,
+        Locality : "Coimbatore"
+
+    },
+    CAREEROBJECTIVE:[
+        "I want to work with a progressive organization where I could utilize my knowledge & skills for mutual growth"
+    ],
+EducationDetails:{
+SSLC : "Secondary from Palaniammal higher secondary school in Kaliyapuram withan aggregated of 91.2%.",
+HSSLC: "Higher secondary from Palaniammal higher secondary school in Kaliyapuram with an aggregated of73%.",
+College : "Bachelor of Computer Application (BCA) from Rathinam College of Arts and Science with an aggregated of 75% ",
+
+},
+PersonalQualified: [
+    "*High motivated and eager to learn new things.",
+    "*Ability to provide the best result",
+    "*Flexible and Adaptable to work in any environment"
+], 
+AreaOfInterest: [ "Web Page Creation, Designing, Marketing and Internet Surfing."],
+technicalSkills: {
+Languages:"HTML&CSS",
+Others : "MS-Word, MS-Excel, MS-PowerPoint and"
+
+},
+ProjectsDone:{
+Topic: "College Club Maintenance",
+FrontEnd: "PHP & HTML",
+BackEnd: "My SQL"
+
+},
+
+certifiedCourse: [
+
+    "Participated in one-day National Level workshop on MATLAB-17 organized by Department of Computer Science at Rathinam College of Arts and Science on 17th February 2017.",
+    "Participated in one-day workshop on “DIGITAL MARKETING” organized by MAZNET solutions"
+],
+personalAchievement:[
+    "Got First place in blind Coding competition held at Angappa College of Arts and Science"
+],
+extracurricularActivity:[
+    "Participated in NSS awareness program (plastic awareness program) held at sulakkal."
+],
+WorkExperience:{
+    WorkExp: "Worked as a Grahic Designer in AG Resources for 3 years"
+
+},
+
+}
+
+];
+//console.log(resumeOfMugesh);
+let resumeOfMugeshJSON = JSON.stringify(resumeOfMugesh);
+console.log(resumeOfMugeshJSON);
---
+++ loop.js
@@ -0,0 +1,34 @@
+//for loop;-
+var obj = [
+    { person: "Mugesh", age: "25", company: "xxx" },
+    { person: "Dinesh", age: "28", company: "yyy" },
+    { person: "Kumar", age: "21", company: "zzz" },
+    ];
+for(let i =0;i<obj.length;i++)
+{
+console.log(obj[i]);
+let propertyJSON = JSON.stringify(obj[i]);
+    console.log(propertyJSON);
+
+}
+//for of loop:-
+
+for(let property of obj)
+{
+console.log(property);
+let propertyJSON = JSON.stringify(property);
+console.log(propertyJSON);
+}
+//for in loop:-
+
+for(let property in obj)
+{
+console.log(obj.[property]);
+let propertyJSON = JSON.stringify(obj[property]);
+    console.log(propertyJSON);
+}
+//for each loop:-
+
+obj.forEach(element => console.log(element));
+
+
